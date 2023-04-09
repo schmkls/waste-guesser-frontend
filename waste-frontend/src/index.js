b85fe22c23cc2757976a7117631d41dac5899080
schmkls <mickels.martin@gmail.com>
2023-04-09 21:42:47 +0200

    improved search

--- waste-frontend/src/index.js
+++ waste-frontend/src/index.js
@@ -8,44 +8,52 @@ import Results from './results/Results';
 
 export default function App() {
 
-  const [image, setImage] = useState();
-  const [tags, setTags] = useState([]);
-  const [ewcGuesses, setEwcGuesses] = useState([]);
+    const [image, setImage] = useState();
+    const [tags, setTags] = useState([]);
+    const [ewcGuesses, setEwcGuesses] = useState([]);
 
-  useEffect(() => {
-    //placeholder for actual guess call
-    fetch('http://127.0.0.1:8000/waste/ewcs/')
-    .then((ewcs) => {
-      ewcs.json()
-    .then((data) => {
-      let guesses = []
-      for (let key in data) {
-        guesses.push({
-          "code": data[key]["code"], 
-          "description": data[key]["description"], 
-          "percentage": 32
-        })
-      }
-      setEwcGuesses(guesses)
-    })  
-    }, (error) => {
-      console.log('error: ', error);
-    })
-    
-    console.log('tags inx: ', tags);
-  }, [tags, image])
+    useEffect(() => {
+        //placeholder for actual guess call
+        console.log('tags: ', tags);
+        let url = 'http://127.0.0.1:8000/waste/simple-guess?'
+        for (const tag of tags) {
+            url = url + 'tags=' + tag + '&'
+        }
 
-  return (
-    <div>
-      <UploadAndDisplayImage onUpload={(img) => setImage(img)}/>
-      <TagsInput 
-        onTags={(words) => setTags(words)}
-        onSingleTag={(tag) => setTags([...tags, tag])}/>
-      <Results 
-        ewcGuesses={ewcGuesses /*placeholder */}
-      />
-    </div>
-  );
+        fetch(url)
+            .then((ewcs) => {
+                ewcs.json()
+                    .then((data) => {
+                        console.log('data: ', data);
+                        let guesses = []
+                        for (let key in data) {
+                            console.log('data[key]: ', data[key]['probability']);
+                            guesses.push({
+                                "code": data[key]['ewc']["code"],
+                                "description": data[key]["ewc"]["description"],
+                                "percentage": data[key]['probability']
+                            })
+                        }
+                        setEwcGuesses(guesses)
+                    })
+            }, (error) => {
+                console.log('error: ', error);
+            })
+
+        console.log('tags inx: ', tags);
+    }, [tags, image])
+
+    return (
+        <div>
+            <UploadAndDisplayImage onUpload={(img) => setImage(img)} />
+            <TagsInput
+                onTags={(words) => setTags(words)}
+                onSingleTag={(tag) => setTags([...tags, tag])} />
+            <Results
+                ewcGuesses={ewcGuesses /*placeholder */}
+            />
+        </div>
+    );
 }
 
 
