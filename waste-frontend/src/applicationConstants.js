249416fe82eb5ff44d5e0615ba754d2cbfee9083
dv20mms <dv20mms@cs.umu.se>
2023-04-25 22:35:23 +0200

    use react barcode reader component, right now only works with qr-codes

--- waste-frontend/src/applicationConstants.js
+++ waste-frontend/src/applicationConstants.js
@@ -1,5 +1,5 @@
 const APPLICATION_CONSTANTS = {
-    BASE_URL : 'http://192.168.50.70:8000/waste'
+    BASE_URL : 'http://192.168.32.4:8000/waste'
 }
 
 export default APPLICATION_CONSTANTS;
--- waste-frontend/src/uploadAndDisplayImage/UploadAndDisplayImage.js
+++ waste-frontend/src/uploadAndDisplayImage/UploadAndDisplayImage.js
@@ -1,101 +1,27 @@
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUpload, faCamera } from '@fortawesome/free-solid-svg-icons';
-import Camera, {FACING_MODES} from 'react-html5-camera-photo';
-import 'react-html5-camera-photo/build/css/index.css';
 import './UploadAndDisplayImage.css';
+import { useZxing } from "react-zxing";
 
 const UploadAndDisplayImage = (props) => {
 
-	const [selectedImage, setSelectedImage] = useState(null);
-	const [takingPhoto, setTakingPhoto] = useState(false);
+	const [result, setResult] = useState("");
+	const { ref } = useZxing({
+		onResult(result) {
+			console.log('result', result);
+			setResult(result.getText());
+		},
+	});
 
-
-	const handleImageUpload = (dataUri) => {
-		// Convert Data URI to Blob
-		const byteString = atob(dataUri.split(',')[1]);
-		const mimeType = dataUri.split(',')[0].split(':')[1].split(';')[0];
-		const arrayBuffer = new ArrayBuffer(byteString.length);
-		const uint8Array = new Uint8Array(arrayBuffer);
-		for (let i = 0; i < byteString.length; i++) {
-		  uint8Array[i] = byteString.charCodeAt(i);
-		}
-		const blob = new Blob([arrayBuffer], { type: mimeType });
-	  
-		// Create File object from Blob with desired file name
-		const fileName = 'image.jpg'; // Replace with the desired file name
-		const file = new File([blob], fileName, { lastModified: new Date().getTime(), type: mimeType });
-	  
-		// Set the File object as the image state
-		props.onUpload(file);
-	};
-
-	if (takingPhoto) {
-		return (
-			<div>
-				<Camera
-					onTakePhoto={(dataUri) => {
-						handleImageUpload(dataUri);
-						setTakingPhoto(false);
-					}}
-					idealFacingMode={FACING_MODES.ENVIRONMENT}
-				/>
-			</div>
-		);
-	}
-
-	if (!selectedImage) {
-		return (
-			<div>
-				<h3 className='uploadInstructions'>
-					Ladda upp bild på produktens streckkod
-				</h3>
-				<div className='bottomStuff'>
-					<div className='imageUpload'>
-						<img
-							className='instructionImage'
-							src="image_upload_instruction.png"
-							alt=""
-						>
-						</img>
-					</div>
-					<div className='buttonsBar'>
-						<label htmlFor='file-input'>
-							<FontAwesomeIcon icon={faUpload} size='4x' />
-						</label>
-						<input
-							id='file-input'
-							type='file'
-							name='myImage'
-							onChange={(event) => {
-								console.log(event.target.files[0]);
-								setSelectedImage(event.target.files[0]);
-								props.onUpload(event.target.files[0])
-							}}
-						/>
-						<FontAwesomeIcon
-							className='camera'
-							icon={faCamera}
-							size='4x'
-							onClick={() => setTakingPhoto(true)}
-						/>
-					</div>
-				</div>
-			</div>
-
-		)
-	}
 	return (
-		<div className='uploaded'>
-			{selectedImage && (
-				<img
-					alt='not found'
-					className='image'
-					src={URL.createObjectURL(selectedImage)}
-				/>
-			)}
-			<button onClick={() => setSelectedImage(null)}>Remove</button>
-		</div>
+		<>
+			<video ref={ref} />
+			<p>
+				<span>Last result:</span>
+				<span>{result}</span>
+			</p>
+		</>
 	);
 };
 
